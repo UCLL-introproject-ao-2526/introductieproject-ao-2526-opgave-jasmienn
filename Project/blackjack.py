ac382594853d0369eaa337c8b59665bb519aee1a
jasmienn <r0263480@ucll.be>
2026-02-28 09:36:29 +0100

    IMG op J,Q,K (1u werk)

--- Project/blackjack.py
+++ Project/blackjack.py
@@ -12,6 +12,7 @@ cards = ['2','3','4', '5', '6', '7', '8', '9', '10','J','Q','K','A']
 one_deck = 4*cards
 decks = 4
 
+# screen
 WIDTH = 600
 HEIGHT = 900
 
@@ -20,10 +21,10 @@ pygame.display.set_caption("Blackjack!")
 fps = 60
 timer = pygame.time.Clock()
 
-font = pygame.font.Font("FreeSansBold.ttf",44)
-font_small = pygame.font.Font("FreeSansBold.ttf",32)
+font = pygame.font.Font("Project/FreeSansBold.ttf",44)
+font_small = pygame.font.Font("Project/FreeSansBold.ttf",32)
 
-#setting deck & hands
+# setting deck & hands
 game_deck = copy.deepcopy(decks*one_deck)
 initial_deal = True
 my_hand=[]
@@ -35,10 +36,18 @@ outcome = 0
 add_score = False
 results = ['','player busted','You win', 'You lose', 'Draw']
 
-#win loss push(draw)
+# win loss push(draw)
 records = [0,0,0]
 player_score = 0
 dealer_score = 0 
+
+# images
+jack_img = pygame.image.load("Project/Images/Jack.png")
+queen_img = pygame.image.load("Project/Images/Queen.png")
+king_img = pygame.image.load("Project/Images/King.png")
+card_x = 70
+card_player_y = 460
+card_dealer_y = 160
 
 # FUNCTIONS
 
@@ -55,23 +64,37 @@ def draw_scores(player,dealer):
     if reveal_dealer:
         screen.blit(font.render(f'Score[{dealer}]', True, 'white'),(350,100)) 
 
+def draw_figure_card(card, i,  x, y):
+    #als J/Q/K > afbeelding
+    if card == 'J':
+        screen.blit(jack_img, ((x + 5 +70*i, y + 10 +5*i)))
+    elif card == 'Q':
+        screen.blit(queen_img, ((x + 5 + 70*i, y+ 10 + 5*i)))
+    elif card == 'K':
+        screen.blit(king_img, ((x + 5 + 70*i, y+ 10 +5*i)))
+
 # draw cards visueel op scherm
 def draw_cards(player, dealer, reveal):
+    
     for i in range(len(player)):
-        pygame.draw.rect(screen,'white', [70 +(70*i), 460 + (5*i), 120,220], 0, 5 )
-        screen.blit(font.render(player[i], True, 'black'), (80 +70*i, 465 +5*i))
-        screen.blit(font.render(player[i], True, 'black'), (80 +70*i, 615 +5*i))
-        pygame.draw.rect(screen,'red', [70 +(70*i), 460 + (5*i),120,220], 5, 5 )
-
+        pygame.draw.rect(screen,'white', [card_x +(70*i), card_player_y + (5*i), 120,220], 0, 5 )
+        screen.blit(font.render(player[i], True, 'black'), (card_x + 10 +70*i, card_player_y + 5 +5*i))
+        screen.blit(font.render(player[i], True, 'black'), (card_x + 10 +70*i, card_player_y +115 +5*i))
+        pygame.draw.rect(screen,'red', [card_x +(70*i), 460 + (5*i),120,220], 5, 5 )
+        #teken figuur op kaart
+        draw_figure_card(player[i], i, card_x, card_player_y)
+    
     # if player hasn't finished turn, dealer will hide one card.
     for i in range(len(dealer)):
-        pygame.draw.rect(screen,'white', [70 +(70*i), 160 + (5*i), 120,220], 0, 5 )
+        pygame.draw.rect(screen,'white', [card_x +(70*i), card_dealer_y + (5*i), 120,220], 0, 5 )
         if i != 0 or reveal:
-            screen.blit(font.render(dealer[i], True, 'black'), (80 +70*i, 165 +5*i))
-            screen.blit(font.render(dealer[i], True, 'black'), (80 +70*i, 315 +5*i))
+            screen.blit(font.render(dealer[i], True, 'black'), (card_x + 10 +70*i, card_dealer_y + 5 +5*i))
+            screen.blit(font.render(dealer[i], True, 'black'), (card_x + 10 +70*i, card_dealer_y + 115 +5*i))
+            draw_figure_card(dealer[i], i, card_x, card_dealer_y)
+            
         else:
-            screen.blit(font.render('???', True, 'black'), (80 +70*i, 165 +5*i))
-            screen.blit(font.render('???', True, 'black'), (80 +70*i, 315 +5*i))
+            screen.blit(font.render('???', True, 'black'), (card_x + 10 +70*i, card_dealer_y + 5 +5*i))
+            screen.blit(font.render('???', True, 'black'), (card_x + 10 +70*i, card_dealer_y + 115 +5*i))
         
         pygame.draw.rect(screen,'blue', [70 +(70*i), 160 + (5*i),120,220], 5, 5 )
 
@@ -124,6 +147,7 @@ def draw_game(act, records, result):
 
         score_text = font_small.render(f'Wins: {records[0]}   Losses: {records[1]}    Draws: {records[2]}', True, 'white')
         screen.blit(score_text,(15,840))
+
     # restart when done playing
     if result != 0:
         screen.blit(font.render(results[result], True, 'white'), (15,25))
@@ -137,6 +161,7 @@ def draw_game(act, records, result):
     return button_list
 
 def check_endgame(hand_act,deal_score,play_score,result,totals, add):
+    # 1: busted 2: win 3: lose 4: draw
     if not hand_act and deal_score >= 17:
         if play_score > 21:
             result = 1
@@ -147,9 +172,9 @@ def check_endgame(hand_act,deal_score,play_score,result,totals, add):
         else:
             result = 4
         if add:
-            if result ==0 or result == 3:
+            if result == 1 or result == 3:
                 totals[1] += 1
-            elif result == 1:
+            elif result == 2:
                 totals[0] += 1
             else:
                 totals[2] += 1
@@ -219,6 +244,7 @@ while run:
                         outcome = 0
                         hand_active = True
                         add_score = True
+                        reveal_dealer = False
                         dealer_score = 0
                         player_score = 0
 
--- Project/logboek.md
+++ Project/logboek.md
@@ -2,3 +2,17 @@
 Hoewel de cursus op 9/2 begon, kan ik nog steeds niet aan de classroomlink. Ik begin dus op mijn computer, zonder te committen. 
 
 Het eerste halfuur van de tutorial zit erop. Het werkt zoals het moet werken tot nu toe.
+
+## 27/02
+
+De blackjack-game-tutorial is af. Er zitten nog enkele bugs in, die zijn voor morgen. 
+ - als ik een nieuw spel begin, staat de dealer op reveal.
+ - controleer de scores
+
+## 28/02 - 8.30
+Tijd voor eigen werk. Ik bedenk eerst graag wat ik wil doen, om dan uit te vogelen hoe ik het doe. 
+- Ik wil vooral mooiere kaarten > https://www.youtube.com/watch?v=rHEnZfq_zEQ 
+
+# 28/04  - 9u30
+Jack, Queen en King hebben nu een mooie kaart.
+
